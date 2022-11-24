2161ff23cc525c86c615d6b74d1ee31aaa12982e
kousei tomiyama <s21c1084dz@s.chibakoudai.jp>
2022-11-24 13:29:07 +0900

    mypkg

---
+++ mypkg/listener.py
@@ -0,0 +1,13 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg)
+
+rclpy.init()
+node = Node("listener")
+sub = node.create_subscription(Person, "person", cb, 10)
+
+rclpy.spin(node)
